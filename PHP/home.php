4d49e1a1ab552c0711015c7de2e1b2166abcc2d6
dharanisakthi <54363478+dharanisakthi@users.noreply.github.com>
2020-08-21 14:33:21 +0530

    Create home.php

---
+++ PHP/home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <title>D'S ELEARNING </title>
+    <link TYPE="TEXT/CSS" REL="STYLESHEET" HREF="css/style.css" >
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <!--D logo next to title -->
+    <link rel="apple-touch-icon" sizes="57x57" href="dlogo/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="dlogo/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="dlogo/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="dlogo/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="dlogo/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="dlogo/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="dlogo/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="dlogo/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="dlogo/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="dlogo/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="dlogo/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="dlogo/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="dlogo/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+  </head>    
+  <body data-spy="scroll" data-target=".navbar" data-offset="50" style="background-image:url('pics/n.jfif');">
+    <!--menu bar-->
+    <div class="topnav">
+      <a class="active" href="home.php">D's ELEARNING</a>  
+      <div class="topnav-right">
+        <a href="reg.php">Register</a>
+        <a href="login.php">Sign in</a>
+      </div>
+    </div> 
+    <div class="container"> 
+      <div class="row" align="center">
+        <img class="img-fluid" src="pics/e.jpg" class="rounded"  height="250" width="250"> 
+      </div>
+      <div class="row" align="center">
+        <h3 class="text-light"><center>Welcome to D's ELearning....<br>Fall in love with learning..<BR></CENTER></h3>
+        <button type="button" class="btn btn-success" onclick="window.location.href = 'reg.php';">Get's started</button>
+      </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  </body>
+ </html>
